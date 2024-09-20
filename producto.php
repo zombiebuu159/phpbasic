4a275c229a2b9e99e0fa4cb0d3c517289ad102fe
zombiebuuu159 <ivanespinoza0226@gmail.com>
2024-09-19 18:16:21 -0600

    areas

---
+++ producto.php
@@ -0,0 +1,35 @@
+<?php include 'includes/header.php'  ?>
+
+<?php
+    $art = $_POST['txtarticulo'];
+    $precio = $_REQUEST['txtprecio'];
+
+    if(isset($_POST['codigo'])){
+        $cod = $_REQUEST['codigo'];
+        switch($cod){
+            case '1':
+                $desc = $precio * .15;
+                $desc1 = $precio - $desc;
+                echo "El articulo es: $art <br>";
+                echo "El codigo es: $cod <br>";
+                echo "El precio con descuento es: $desc1 <br>";
+                echo "El precio original es: $precio";
+            break;
+            case '2':
+                $desc = $precio * .30;
+                $desc1 = $precio - $desc;
+                echo "El articulo es: $art <br>";
+                echo "El codigo es: $cod <br>";
+                echo "El precio con descuento es: $desc1 <br>";
+                echo "El precio original es: $precio";
+            break;
+            default:
+                echo "No hay opcion";
+            break; 
+
+        }
+
+    }
+?>
+
+<?php include 'includes/footer.php'  ?>
